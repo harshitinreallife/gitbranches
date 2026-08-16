5884349421d19c4b54ddd1559b698339b0d30d14
harshitinreallife <harshitinreallife@gmail.com>
2026-08-16 15:23:19 +0530

    Second

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ let h = document.querySelector("h1")
 h.addEventListener("click",function(){
     h.style.backgroundColor = "red";
 })
+h.addEventListener("click",function(){
+    h.style.backgroundColor = "blue";
+})
